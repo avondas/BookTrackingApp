56b6e37b98b099680b43092a67dfc47d49890fa2
avondas <aviendahooft@hotmail.com>
2024-02-25 22:26:38 +0100

    added author+title input fields

--- booktrackingapp/app/src/main/java/com/example/booktrackingapp/AddBookFragment.kt
+++ booktrackingapp/app/src/main/java/com/example/booktrackingapp/AddBookFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.booktrackingapp.databinding.FragmentAddBookBinding
@@ -11,6 +12,8 @@ import com.example.booktrackingapp.databinding.FragmentStartBinding
 
 class AddBookFragment : Fragment() {
     private var _binding: FragmentAddBookBinding? = null
+    lateinit var authorInput: EditText
+    lateinit var titleInput: EditText
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,16 +23,19 @@ class AddBookFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentAddBookBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        authorInput = requireView().findViewById(R.id.bookAuthor)
+        titleInput = requireView().findViewById(R.id.bookTitle)
         binding.addButton.setOnClickListener {
+//            println(authorInput.text)
+//            println(titleInput.text)
             findNavController().navigate(R.id.action_AddBookFragment_to_HomeFragment)
+
         }
     }
 
